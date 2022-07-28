fbf71d8df18e9ab97c7bd379ef07d4e7676854ba
tahaebaed <tahaaebaed2@gmail.com>
2022-07-28 03:54:12 +0200

    upload task

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import SideBar from './layout/SideBar'
+
+function App() {
+  return (
+    <div className='App container mx-auto'>
+      <SideBar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/layout/SideBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div className='bg-white h-screen w-72'>
+      <header className='py-3'>
+        <h1 className='text-2xl text-orange-500 font-bold'>
+          Task Manger <span className='text-black'>App</span>
+        </h1>
+      </header>
+    </div>
+  )
+}
+
+export default SideBar
